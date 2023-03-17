ef8b7c95a9219a0b371a3b1d79a5431bbcca0ea7
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-17 18:55:40 +0100

    ___

---
+++ utils/cloneAll.js
@@ -0,0 +1,15 @@
+const cloneAll = async (username) => {
+  console.log(`Cloning all repositories for ${username}...`);
+  const repos = await getRepositories(username);
+  console.log(`Found ${repos.length} repositories:`);
+  let i = 1;
+  for (const repo of repos) {
+    console.log(`Cloning ${repo.name} (${i}/${repos.length})...`);
+    await cloneRepository(username, repo.name);
+    console.log(`Done cloning ${repo.name}`);
+    i++;
+  }
+  console.log(`Done cloning all repositories for ${username}`);
+};
+
+export default cloneAll;
