56c3014383d85fdb85dc04305243fcbf69e75055
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-17 22:25:10 +0100

    ...

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import { argv } from "process";
+import cloneAll from "./utils/cloneAll";
+import cloneRepository from "./utils/cloneRepository";
+
+if (argv[2] === "clone") {
+  const username = argv[3];
+  const allFlagIndex = argv.indexOf("--all");
+  if (allFlagIndex > -1) {
+    cloneAll(username);
+  } else {
+    const repos = argv.slice(4);
+    for (const repo of repos) {
+      cloneRepository(username, repo)
+        .then(() => {
+          console.log(`Done cloning ${repo}`);
+        })
+        .catch((err) => {
+          console.error(`Error cloning ${repo}: ${err}`);
+        });
+    }
+  }
+}
