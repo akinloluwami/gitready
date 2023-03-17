3799a472bd5ce617e9d21b3c4b43f9c50cf00024
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-17 19:31:20 +0100

    spin log progress

--- utils/cloneAll.js
+++ utils/cloneAll.js
@@ -1,13 +1,14 @@
 const getRepositories = require("./getRepositories");
 const cloneRepository = require("./cloneRepository");
+const logSpinner = require("./logSpinner");
 
 const cloneAll = async (username) => {
-  console.log(`Cloning all repositories for ${username}...`);
+  logSpinner(`Cloning all repositories for ${username}...`);
   const repos = await getRepositories(username);
   console.log(`Found ${repos.length} repositories:`);
   let i = 1;
   for (const repo of repos) {
-    console.log(`Cloning ${repo.name} (${i}/${repos.length})...`);
+    logSpinner(`Cloning ${repo.name} (${i}/${repos.length})...`);
     await cloneRepository(username, repo.name);
     console.log(`Done cloning ${repo.name}`);
     i++;
