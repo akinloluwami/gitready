69678a77b3798c6e638d7360b9c4e6b8105ce345
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-17 18:04:01 +0100

    ...

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 #!/usr/bin/env node
 
+const { performance } = require("perf_hooks");
+const program = require("commander");
 const simpleGit = require("simple-git");
-const args = process.argv.slice(2);
 
-if (args[0] === "clone") {
-  const username = args[1];
-  const repos = args.slice(2);
+program
+  .version("1.0.0")
+  .command("clone <username> [repos...]")
+  .action(async (username, repos) => {
+    const startTime = performance.now();
+    console.log(`Starting to clone ${repos.length} repositories...`);
 
-  repos.forEach((repoName) => {
-    const repoUrl = `https://github.com/${username}/${repoName}.git`;
-    simpleGit().clone(repoUrl);
+    const git = simpleGit();
+    git.outputHandler((command, stdout, stderr) => {
+      stdout.on("data", (chunk) => {
+        const logLine = chunk.toString().trim();
+        if (logLine.startsWith("Cloning into")) {
+          console.log(`${logLine} (${git.progress() * 100}%)`);
+        }
+      });
+    });
+
+    for (const repo of repos) {
+      console.log(`Cloning ${repo}...`);
+      await git.clone(`https://github.com/${username}/${repo}.git`);
+      console.log(`Done cloning ${repo}`);
+    }
+
+    const endTime = performance.now();
+    const timeTaken = ((endTime - startTime) / 1000).toFixed(2);
+    console.log(`Done cloning all in ${timeTaken} seconds`);
   });
-} else {
-  console.error(`Unknown command: ${args[0]}`);
-  process.exit(1);
-}
+
+program.parse(process.argv);
