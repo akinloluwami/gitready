4a3d6a57a592673b722d911f0c0b8f70470ead3d
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-18 02:48:45 +0100

    ..

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+#!/usr/bin/env node
+const program = require("commander");
 const { argv } = require("process");
 const cloneAll = require("./utils/cloneAll");
 const cloneRepository = require("./utils/cloneRepository");
+
+program
+  .version("1.0.0")
+  .description("A simple CLI tool")
+  .option("-a, --action <action>", "Action to perform")
+  .option("-f, --file <file>", "File to process")
+  .action(() => {
+    const file = fs.readFileSync(program.file, "utf8");
+    console.log(`Performing "${program.action}" on file "${file}".`);
+  });
+
+program.parse(process.argv);
 
 if (argv[2] === "clone") {
   const username = argv[3];
