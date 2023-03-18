94b0805cc1d6901f704e88e4f5db4375832c74e8
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-18 04:11:57 +0100

    pain

---
+++ src/utils/index.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+// const { argv } = require("process");
+// const cloneAll = require("./utils/cloneAll");
+// const cloneRepository = require("./utils/cloneRepository");
+
+import { argv } from "process";
+import cloneRepository from "./utils/cloneRepository";
+import cloneAll from "./utils/cloneAll.mjs.js.js.js.js.js.js";
+
+if (argv[2] === "clone") {
+  const username = argv[3];
+  const allFlagIndex = argv.indexOf("--all");
+  if (allFlagIndex > -1) {
+    cloneAll(username);
+  } else {
+    const repos = argv.slice(4);
+    for (const repo of repos) {
+      cloneRepository(username, repo)
+        .then(() => {
+          console.log(`Done cloning ${repo}`);
+        })
+        .catch((err) => {
+          console.error(`Error cloning ${repo}: ${err}`);
+        });
+    }
+  }
+}
