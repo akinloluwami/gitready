4d9c0dde2b5b7a7aad7c07733e5dd947c514371d
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-18 01:51:11 +0100

    check if repo has been cloned

--- utils/cloneRepository.js
+++ utils/cloneRepository.js
@@ -1,9 +1,21 @@
 const { exec } = require("child_process");
+const path = require("path");
+const fs = require("fs");
 
 const cloneRepository = async (username, repo) => {
+  const repoPath = path.join(process.cwd(), repo);
+
+  if (fs.existsSync(repoPath)) {
+    console.log(`${repo} has already been cloned`);
+    return;
+  }
+
   return new Promise((resolve, reject) => {
     exec(
       `git clone https://github.com/${username}/${repo}`,
+      {
+        cwd: process.cwd(),
+      },
       (err, stdout, stderr) => {
         if (err) {
           reject(err);
